8be358bef08d4a477b930d6f4631087bb80c9ee5
Martin Holst Swende <martin@swende.se>
2023-10-24 19:29:42 +0200

    testing: add TB interface, add missing methods to B

--- testing/b.go
+++ testing/b.go
@@ -6,6 +6,25 @@ import (
 
 type B struct{}
 
+func (b *B) Cleanup(f func())                  {}
+func (b *B) Error(args ...any)                 {}
+func (b *B) Errorf(format string, args ...any) {}
+func (b *B) Fail()                             {}
+func (b *B) FailNow()                          {}
+func (b *B) Failed() bool                      { panic("not implemented") }
+func (b *B) Fatal(args ...any)                 {}
+func (b *B) Fatalf(format string, args ...any) {}
+func (b *B) Helper()                           {}
+func (b *B) Log(args ...any)                   {}
+func (b *B) Logf(format string, args ...any)   {}
+func (b *B) Name() string                      { panic("not implemented") }
+func (b *B) Setenv(key, value string)          {}
+func (b *B) Skip(args ...any)                  {}
+func (b *B) SkipNow()                          {}
+func (b *B) Skipf(format string, args ...any)  {}
+func (b *B) Skipped() bool                     { panic("not implemented") }
+func (b *B) TempDir() string                   { panic("not implemented") }
+
 func (b *B) StartTimer()                         {}
 func (b *B) StopTimer()                          {}
 func (b *B) ResetTimer()                         {}
---
+++ testing/interfaces.go
@@ -0,0 +1,28 @@
+package testing
+
+// compile-time conformance checks
+var (
+	_ = TB((*T)(nil))
+	_ = TB((*B)(nil))
+)
+
+type TB interface {
+	Cleanup(func())
+	Error(args ...any)
+	Errorf(format string, args ...any)
+	Fail()
+	FailNow()
+	Failed() bool
+	Fatal(args ...any)
+	Fatalf(format string, args ...any)
+	Helper()
+	Log(args ...any)
+	Logf(format string, args ...any)
+	Name() string
+	Setenv(key, value string)
+	Skip(args ...any)
+	SkipNow()
+	Skipf(format string, args ...any)
+	Skipped() bool
+	TempDir() string
+}
