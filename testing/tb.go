3ca5388a63a999c731d46f4e44f586ad80ff74bf
Adam Korczynski <adam@adalogics.com>
2024-09-06 13:35:22 +0100

    rb

--- testing/tb.go
+++ testing/tb.go
@@ -1,22 +1,22 @@
 package testing
 
-type TB struct{}
-
-func (c TB) Cleanup(func())                    {}
-func (c TB) Error(args ...any)                 {}
-func (c TB) Errorf(format string, args ...any) {}
-func (c TB) Fail()                             {}
-func (c TB) FailNow()                          {}
-func (c TB) Failed() bool                      { return false }
-func (c TB) Fatal(args ...any)                 {}
-func (c TB) Fatalf(format string, args ...any) {}
-func (c TB) Helper()                           {}
-func (c TB) Log(args ...any)                   {}
-func (c TB) Logf(format string, args ...any)   {}
-func (c TB) Name() string                      { return "Fuzz" }
-func (c TB) Setenv(key, value string)          {}
-func (c TB) Skip(args ...any)                  {}
-func (c TB) SkipNow()                          {}
-func (c TB) Skipf(format string, args ...any)  {}
-func (c TB) Skipped() bool                     { return true }
-func (c TB) TempDir() string                   { return "FUZZ" }
+type TB interface {
+	Cleanup(func())
+	Error(args ...any)
+	Errorf(format string, args ...any)
+	Fail()
+	FailNow()
+	Failed() bool
+	Fatal(args ...any)
+	Fatalf(format string, args ...any)
+	Helper()
+	Log(args ...any)
+	Logf(format string, args ...any)
+	Name() string
+	Setenv(key, value string)
+	Skip(args ...any)
+	SkipNow()
+	Skipf(format string, args ...any)
+	Skipped() bool
+	TempDir() string
+}
